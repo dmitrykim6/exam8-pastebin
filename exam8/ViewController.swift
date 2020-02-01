50b5d3d8e5f60ef67432ef2e63525e44c5a69c7e
Dmitry <>
2020-02-01 11:24:51 +0600

    init commit

---
+++ exam8/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  exam8
+//
+//  Created by Dmitry on 01/02/2020.
+//  Copyright © 2020 Dmitry. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
